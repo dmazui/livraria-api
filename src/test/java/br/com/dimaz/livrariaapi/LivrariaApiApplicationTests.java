03bc0e9711a937c0197acbc7cba948f6960706de
DevDiogo <dev.diogo@gmail.com>
2020-06-28 20:22:01 -0300

    fst

---
+++ src/test/java/br/com/dimaz/livrariaapi/LivrariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dimaz.livrariaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
